3393006c05f05fb4b667ff18d0ecbbc53e4724d5
marusama <maru.sama@gmail.com>
2017-12-01 17:01:57 +0300

    Dropbox semaphores

--- semaphore_test.go
+++ semaphore_test.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	xsync "golang.org/x/sync/semaphore"
+	"github.com/dropbox/godropbox/sync2"
 )
 
 func checkLimit(t *testing.T, sem Semaphore, expected int) {
@@ -873,6 +874,112 @@ func BenchmarkXSyncSemaphore_Acquire_Release_over_limit(b *testing.B) {
 			for j := 0; j < b.N; j++ {
 				sem.Acquire(ctx, 1)
 				sem.Release(1)
+			}
+			wg.Done()
+		}()
+	}
+
+	b.ResetTimer()
+	close(c) // start
+	wg.Wait()
+}
+
+func BenchmarkDropboxBoundedSemaphore_Acquire_Release_under_limit_simple(b *testing.B) {
+	sem := sync2.NewBoundedSemaphore(uint(b.N))
+
+	for i := 0; i < b.N; i++ {
+		sem.Acquire()
+		sem.Release()
+	}
+}
+
+func BenchmarkDropboxBoundedSemaphore_Acquire_Release_under_limit(b *testing.B) {
+	sem := sync2.NewBoundedSemaphore(100)
+
+	c := make(chan struct{})
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			<-c
+			for j := 0; j < b.N; j++ {
+				sem.Acquire()
+				sem.Release()
+			}
+			wg.Done()
+		}()
+	}
+
+	b.ResetTimer()
+	close(c) // start
+	wg.Wait()
+}
+
+func BenchmarkDropboxBoundedSemaphore_Acquire_Release_over_limit(b *testing.B) {
+	sem := sync2.NewBoundedSemaphore(10)
+
+	c := make(chan struct{})
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			<-c
+			for j := 0; j < b.N; j++ {
+				sem.Acquire()
+				sem.Release()
+			}
+			wg.Done()
+		}()
+	}
+
+	b.ResetTimer()
+	close(c) // start
+	wg.Wait()
+}
+
+func BenchmarkDropboxUnboundedSemaphore_Acquire_Release_under_limit_simple(b *testing.B) {
+	sem := sync2.NewUnboundedSemaphore(b.N)
+
+	for i := 0; i < b.N; i++ {
+		sem.Acquire()
+		sem.Release()
+	}
+}
+
+func BenchmarkDropboxUnboundedSemaphore_Acquire_Release_under_limit(b *testing.B) {
+	sem := sync2.NewUnboundedSemaphore(100)
+
+	c := make(chan struct{})
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			<-c
+			for j := 0; j < b.N; j++ {
+				sem.Acquire()
+				sem.Release()
+			}
+			wg.Done()
+		}()
+	}
+
+	b.ResetTimer()
+	close(c) // start
+	wg.Wait()
+}
+
+func BenchmarkDropboxUnboundedSemaphore_Acquire_Release_over_limit(b *testing.B) {
+	sem := sync2.NewUnboundedSemaphore(10)
+
+	c := make(chan struct{})
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			<-c
+			for j := 0; j < b.N; j++ {
+				sem.Acquire()
+				sem.Release()
 			}
 			wg.Done()
 		}()
